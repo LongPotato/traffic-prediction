043139264f74a4756774b544136fd8135b869494
Miraj Patel <mspatel@cpp.edu>
2017-03-06 15:59:28 -0800

    added training set code

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, send_file, request
 from flask import json
 
+from sklearn import linear_model
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
 app = Flask(__name__)
+
+input = []
+output = []
 
 @app.route("/")
 def index():
@@ -11,6 +19,14 @@ def index():
 def train():
     data = json.loads(request.data.decode())
     id = data["roadId"]
+
+    input.append([data["roadId"], data["direction"],data["day"],data["time"]])
+    output.extend(data["traffic"])
+    ## put this when predicting with the input
+#    model = make_pipeline(PolynomialFeatures(2), Ridge())
+ #   model.fit(input, output)
+
+
     return id
 
 
